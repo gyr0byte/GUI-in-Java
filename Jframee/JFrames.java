11ebeafdf8336b9dcedc68f1e87eb4a9da564224
Gauravlovescode <gqurav69@gmail.com>
2026-01-21 18:00:58 +0545

    Refactor JFrames class to include JFrame initialization and visibility settings

--- Jframee/JFrames.java
+++ Jframee/JFrames.java
@@ -1,4 +1,8 @@
-
+import javax.swing.JFrame;
 public class JFrames {
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setVisible(true);
+    }
 }
