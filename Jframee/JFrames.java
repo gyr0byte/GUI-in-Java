865e3832ab58ff1eff2b7af65f03ba305e5e4de3
Gauravlovescode <gqurav69@gmail.com>
2026-01-21 18:13:32 +0545

    Enhance JFrames class by adding title, close operation, and size settings for the JFrame

--- Jframee/JFrames.java
+++ Jframee/JFrames.java
@@ -1,8 +1,10 @@
 import javax.swing.JFrame;
 public class JFrames {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-
-        frame.setVisible(true);
+        JFrame frame = new JFrame(); // creates a frame
+        frame.setTitle("JFrame title goes here"); // sets the title for the JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+        frame.setSize(640, 420); // sets the x dimension and y dimension of the frame
+        frame.setVisible(true); // make frame visible
     }
 }
