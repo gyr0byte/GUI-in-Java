c2e4aec084fab6d5c280b2c650e7170d78d4f875
Gauravlovescode <gqurav69@gmail.com>
2026-01-22 09:10:10 +0545

    Refactor code structure for improved readability and maintainability

---
+++ Label/LabelDemo.java
@@ -0,0 +1,21 @@
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class LabelDemo {
+    public static void main(String[] args) {
+        ImageIcon image = new ImageIcon("/Label/logo.jpg");
+        JLabel label = new JLabel();
+        label.setText("I am Gaurav");
+        label.setIcon(image);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(640, 420);
+        frame.add(label);
+        frame.setVisible(true);
+        frame.add(label);
+    }
+
+}
