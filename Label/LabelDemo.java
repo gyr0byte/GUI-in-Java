76579a4bdca584166326d85d3cdcf1dc68139d13
Gauravlovescode <gqurav69@gmail.com>
2026-01-23 21:03:02 +0545

    Fix image path and update JFrame settings for improved layout

--- Label/LabelDemo.java
+++ Label/LabelDemo.java
@@ -5,14 +5,14 @@ import javax.swing.JLabel;
 
 public class LabelDemo {
     public static void main(String[] args) {
-        ImageIcon image = new ImageIcon("/Label/logo.jpg");
+        ImageIcon image = new ImageIcon("Label/logo.jpg");
         JLabel label = new JLabel();
         label.setText("I am Gaurav");
         label.setIcon(image);
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(640, 420);
+        frame.setResizable(true);
+        frame.setSize(900, 720);
         frame.add(label);
         frame.setVisible(true);
         frame.add(label);
