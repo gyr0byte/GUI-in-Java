740135e504b656eeefe57764202854aef7569013
Gauravlovescode <gqurav69@gmail.com>
2026-01-26 21:41:23 +0545

    Enhance JavaPanel setup by adding a red JPanel with dark gray background and adjusting JFrame size

--- Panels/JavaPanel.java
+++ Panels/JavaPanel.java
@@ -1,13 +1,21 @@
 
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class JavaPanel {
     public static void main(String[] args) {
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.DARK_GRAY);
+        redPanel.setBounds(0,0,250,250);
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(750, 480);
         frame.setVisible(true);
         frame.setLayout(null);
+        frame.add(redPanel);
     }
 }
