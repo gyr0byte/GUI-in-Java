47a8ae075c067f5fbc625e7d2c17bfaaf62c7365
Gauravlovescode <gqurav69@gmail.com>
2026-01-26 22:08:54 +0545

    Add JLabel with icon to greyPanel and update layout in JavaPanel

--- Panels/JavaPanel.java
+++ Panels/JavaPanel.java
@@ -7,28 +7,30 @@ import javax.swing.JPanel;
 
 public class JavaPanel {
     public static void main(String[] args) {
-        ImageIcon icon = new ImageIcon();
+        ImageIcon icon = new ImageIcon("Panels/cat.png");
 
         JLabel label = new JLabel();
         label.setText("Hi");
+        label.setIcon(icon);
 
         JPanel greyPanel = new JPanel();
         greyPanel.setBackground(Color.DARK_GRAY);
         greyPanel.setBounds(0, 0, 250, 250);
-
+        
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.BLUE);
         bluePanel.setBounds(250, 0, 250, 250);
-
+        
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0, 250, 500, 250);
-
+        
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(750, 480);
         frame.setVisible(true);
         frame.setLayout(null);
+        greyPanel.add(label);
         frame.add(greyPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
