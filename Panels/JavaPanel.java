dc7f653bf3d4f328b06a103a7cefb7c51265e624
Gauravlovescode <gqurav69@gmail.com>
2026-01-26 21:32:56 +0545

    Add initial JavaPanel class with JFrame setup for GUI

---
+++ Panels/JavaPanel.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame;
+
+
+public class JavaPanel {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setLayout(null);
+    }
+}
