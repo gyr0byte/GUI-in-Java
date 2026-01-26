ac5f9acb816ddc580b386a56b8598c6983e0f1ec
Gauravlovescode <gqurav69@gmail.com>
2026-01-26 21:50:44 +0545

    Refactor JavaPanel to replace redPanel with greyPanel and ensure proper layout setup

--- Panels/JavaPanel.java
+++ Panels/JavaPanel.java
@@ -3,19 +3,24 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class JavaPanel {
     public static void main(String[] args) {
 
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.DARK_GRAY);
-        redPanel.setBounds(0,0,250,250);
+        JPanel greyPanel = new JPanel();
+        greyPanel.setBackground(Color.DARK_GRAY);
+        greyPanel.setBounds(0, 0, 250, 250);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.BLUE);
+        bluePanel.setBounds(250, 0, 250, 250);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(750, 480);
         frame.setVisible(true);
         frame.setLayout(null);
-        frame.add(redPanel);
+        frame.add(greyPanel);
+        frame.add(bluePanel);
+
     }
 }
