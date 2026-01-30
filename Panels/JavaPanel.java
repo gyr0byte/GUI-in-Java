03bbc01c57105bcbd92dba73c4c1e31723a64e01
Gauravlovescode <gqurav69@gmail.com>
2026-01-30 21:33:18 +0545

    Refactor panel layouts to use null layout and move label to greenPanel for better alignment

--- Panels/JavaPanel.java
+++ Panels/JavaPanel.java
@@ -1,5 +1,4 @@
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -15,28 +14,29 @@ public class JavaPanel {
         label.setIcon(icon);
         label.setVerticalAlignment(JLabel.TOP);
         label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setBounds(32, 0, 150, 150);
 
         JPanel greyPanel = new JPanel();
         greyPanel.setBackground(Color.DARK_GRAY);
         greyPanel.setBounds(0, 0, 250, 250);
-        greyPanel.setLayout(new BorderLayout());
-        
+        greyPanel.setLayout(null);
+
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.BLUE);
         bluePanel.setBounds(250, 0, 250, 250);
-        bluePanel.setLayout(new BorderLayout());
-        
+        bluePanel.setLayout(null);
+
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0, 250, 500, 250);
-        greenPanel.setLayout(new BorderLayout());
-        
+        greenPanel.setLayout(null);
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(750, 550);
         frame.setVisible(true);
         frame.setLayout(null);
-        bluePanel.add(label);
+        greenPanel.add(label);
         frame.add(greyPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
