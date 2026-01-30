778cf7fc9493125d3a8a584d20c37846fb5c4ef2
Gauravlovescode <gqurav69@gmail.com>
2026-01-30 21:30:35 +0545

    Fix label placement by moving it to bluePanel and updating layouts for greyPanel and bluePanel

--- Panels/JavaPanel.java
+++ Panels/JavaPanel.java
@@ -14,14 +14,17 @@ public class JavaPanel {
         label.setText("Hi");
         label.setIcon(icon);
         label.setVerticalAlignment(JLabel.TOP);
+        label.setHorizontalAlignment(JLabel.RIGHT);
 
         JPanel greyPanel = new JPanel();
         greyPanel.setBackground(Color.DARK_GRAY);
         greyPanel.setBounds(0, 0, 250, 250);
+        greyPanel.setLayout(new BorderLayout());
         
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.BLUE);
         bluePanel.setBounds(250, 0, 250, 250);
+        bluePanel.setLayout(new BorderLayout());
         
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
@@ -33,7 +36,7 @@ public class JavaPanel {
         frame.setSize(750, 550);
         frame.setVisible(true);
         frame.setLayout(null);
-        greenPanel.add(label);
+        bluePanel.add(label);
         frame.add(greyPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
