20c6c673b85cc74abb81766311b1e2673d8cd4e5
Gauravlovescode <gqurav69@gmail.com>
2026-01-30 21:26:46 +0545

    Update JavaPanel layout and label alignment for improved UI

--- Panels/JavaPanel.java
+++ Panels/JavaPanel.java
@@ -1,4 +1,5 @@
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -12,6 +13,7 @@ public class JavaPanel {
         JLabel label = new JLabel();
         label.setText("Hi");
         label.setIcon(icon);
+        label.setVerticalAlignment(JLabel.TOP);
 
         JPanel greyPanel = new JPanel();
         greyPanel.setBackground(Color.DARK_GRAY);
@@ -24,10 +26,11 @@ public class JavaPanel {
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0, 250, 500, 250);
+        greenPanel.setLayout(new BorderLayout());
         
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.setSize(750, 480);
+        frame.setSize(750, 550);
         frame.setVisible(true);
         frame.setLayout(null);
         greenPanel.add(label);
