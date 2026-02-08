bd20775b3ced52b241454339806058abec1b6aa7
Gauravlovescode <gqurav69@gmail.com>
2026-02-08 16:45:19 +0545

    Update MyFramee class: adjust button size, add icon, and change action output

--- Buttonss/MyFramee.java
+++ Buttonss/MyFramee.java
@@ -1,18 +1,23 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFramee extends JFrame implements ActionListener{
     JButton button;
     public MyFramee(){
+        ImageIcon icon = new ImageIcon("Panels/cat.png");
+
+
         button = new JButton();
-        button.setBounds(200,100, 150,50);
+        button.setBounds(200,100, 250,150);
         button.addActionListener(this);
         button.setText("press me");
         button.setFocusable(false);
-        
+        button.setIcon(icon);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
@@ -23,7 +28,7 @@ public class MyFramee extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button){
-            System.out.println("lol");
+            System.out.println("meow");
         }
     }
 }
