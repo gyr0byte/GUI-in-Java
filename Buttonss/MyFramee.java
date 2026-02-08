4d1fea7e2ce4c6a5cc5d030cee7d4e5319885f1d
Gauravlovescode <gqurav69@gmail.com>
2026-02-08 16:13:20 +0545

    Refactor project structure: update source paths, implement MyFramee class, and remove obsolete MyFrame class

---
+++ Buttonss/MyFramee.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JFrame;
+
+public class MyFramee extends JFrame{
+
+    public MyFramee(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
+}
