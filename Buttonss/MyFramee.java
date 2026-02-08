afadf80c932e8c38a43881ea271cbbdd73135d4c
Gauravlovescode <gqurav69@gmail.com>
2026-02-08 16:20:37 +0545

    Implement ActionListener in MyFramee class and add button with action handling

--- Buttonss/MyFramee.java
+++ Buttonss/MyFramee.java
@@ -1,12 +1,24 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFramee extends JFrame{
+public class MyFramee extends JFrame implements ActionListener{
 
     public MyFramee(){
+        JButton button = new JButton();
+        button.setBounds(200,100, 150,50);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
 }
